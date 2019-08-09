de61566c2e72edd769444fe1156eb28eb7f96730
Greg Intermaggio <g.intermaggio@gmail.com>
2019-08-09 18:44:44 +0000

    Add rollback to cascade delete migration

--- database/migrations/2019_08_09_165803_cascade_delete_existing_models.php
+++ database/migrations/2019_08_09_165803_cascade_delete_existing_models.php
@@ -63,5 +63,41 @@ class CascadeDeleteExistingModels extends Migration
     public function down()
     {
         //
+
+        Schema::table('meetings', function (Blueprint $table) {
+            //
+            $table->dropForeign('meetings_program_id_foreign');
+            $table->foreign('program_id')->references('id')->on('programs');
+        });
+
+        Schema::table('contributors', function (Blueprint $table) {
+            //
+            $table->dropForeign('contributors_program_id_foreign');
+            $table->foreign('program_id')->references('id')->on('programs');
+
+            $table->dropForeign('contributors_organization_id_foreign');
+            $table->foreign('organization_id')->references('id')->on('programs');
+        });
+
+        Schema::table('organization_user', function (Blueprint $table) {
+            //
+            $table->dropForeign('organization_user_user_id_foreign');
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->dropForeign('organization_user_organization_id_foreign');
+            $table->foreign('organization_id')->references('id')->on('organizations');
+        });
+
+        Schema::table('templates', function (Blueprint $table) {
+            //
+            $table->dropForeign('templates_organization_id_foreign');
+            $table->foreign('organization_id')->references('id')->on('organizations');
+        });
+
+        Schema::table('locations', function (Blueprint $table) {
+            //
+            $table->dropForeign('locations_site_id_foreign');
+            $table->foreign('site_id')->references('id')->on('sites');
+        });
     }
 }

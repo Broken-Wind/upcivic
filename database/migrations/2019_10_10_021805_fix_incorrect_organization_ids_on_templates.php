04e8a34962e4231d960f8f462915698839c12203
Greg Intermaggio <g.intermaggio@gmail.com>
2019-10-21 20:46:34 -0700

    Fix incorrect organization ids on templates

---
+++ database/migrations/2019_10_10_021805_fix_incorrect_organization_ids_on_templates.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class FixIncorrectOrganizationIdsOnTemplates extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        $templates = DB::table('templates')->get();
+
+        foreach ($templates as $template) {
+            $correctOrganizationId =  DB::table('tenants')->find($template->organization_id)->organization_id;
+            DB::table('templates')
+                ->where('id', $template->id)
+                ->update(['organization_id' => $correctOrganizationId]);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}

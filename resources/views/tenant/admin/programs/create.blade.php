b46b2c988a80a079c7199aa14e4661914ad9bedb
Greg Intermaggio <g.intermaggio@gmail.com>
2020-08-29 21:24:35 -0700

    Button language

--- resources/views/tenant/admin/programs/create.blade.php
+++ resources/views/tenant/admin/programs/create.blade.php
@@ -74,7 +74,7 @@
                     </div>
                 </div>
                 <div class="form-group">
-                    <button type="submit" class="btn btn-primary">Review Details</button>
+                    <button type="submit" class="btn btn-primary">Add & Review</button>
                 </div>
             </form>
         </div>

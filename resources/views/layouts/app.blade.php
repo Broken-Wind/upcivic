959421ed5db1ab2b98aefe463b1f042d5f77d7cf
Greg Intermaggio <g.intermaggio@gmail.com>
2020-09-04 15:54:14 -0700

    Add confirm dialog when regenerating demo data

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
                                 <li class="nav-item">
                                 <form method="POST" action="{{ tenant()->route('tenant:admin.demo.store') }}">
                                     @csrf
-                                    <button type="submit" class="btn btn-primary">REGENERATE DEMO DATA</button>
+                                    <button type="submit" class="btn btn-primary mr-3" onClick="return confirm('Are you sure?')">REGENERATE DEMO DATA</button>
                                 </form>
                                 </li>
                             @endif

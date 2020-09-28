97283edf38eaa3dd3235be7f193395ce719f2367
calinf <calin.furau@gmail.com>
2020-09-28 17:12:50 -0400

    UI tweaks for Instructors tasks

--- resources/views/tenant/admin/organizations/outgoing_assignments/index.blade.php
+++ resources/views/tenant/admin/organizations/outgoing_assignments/index.blade.php
@@ -14,14 +14,20 @@
     <div class="card mb-3">
         <div class="card-header"><strong>General</strong></div>
         <div class="card-body">
-            @foreach(['1', '2', '3'] as $task)
+            @foreach(['Sign a contract', 'Provide a copy of your liability insurance policy', '3'] as $task)
                 @include('tenant.admin.organizations.components.assignment_list_row', ['taks' => $task])
             @endforeach
         </div>
     </div>
     <div class="card mb-3">
-        <div class="card-header"><strong>Annette Meyer</strong></div>
-        <div class="card-body"></div>
+        <div class="card-header"><strong>Instructors</strong></div>
+        <div class="card-body">
+            <h5 class="card-title text-muted">Ilene Reese</h5>
+            @foreach(['TB Test', 'Affidavit', 'Background check'] as $task)
+                @include('tenant.admin.organizations.components.assignment_list_row', ['taks' => $task])
+            @endforeach
+            <h5 class="card-title pb-1 text-muted">Rudy Morin</h5>
+        </div>
     </div>
 </div>
 @endsection

eab438436d3f1e54dc51484be5ad8c99ce699fff
calinf <calin.furau@gmail.com>
2020-09-28 15:14:46 -0400

    Add assignment list row reusable component

---
+++ resources/views/tenant/admin/organizations/components/assignment_list_row.blade.php
@@ -0,0 +1,29 @@
+<div class="container">
+
+    <div class="row">
+        <div class="col-2">
+            "{{ $taks }}"
+        </div>
+        <div class="col-6"> Bla provide everything that is needed </div>
+        <div class="col-2 text-center">
+            <div class="alert-success font-weight-bold">Done</div>
+        </div>
+        <div class="col-2 text-center">
+            <input type="checkbox" class="form-controll" id="exampleCheck1" onClick="return confirm('Mark as Done?')"/>
+            <i class="far fa-bell mx-3"></i>
+            <i class="far fa-trash-alt"></i>
+        </div>
+    </div>
+
+    <div class="row my-4">
+        <div class="col-3 border-right">
+            <a href="/images/myw3schoolsimage.jpg" download="originalContract">
+                originalContractFile.pdf <i class="fas fa-file-download fa-lg mx-2"></i> 
+            </a>
+        </div>
+        <div class="col-3">
+            Missing File <i class="fas fa-file-upload fa-lg mx-2"> </i>
+        </div>
+    </div>
+
+</div>

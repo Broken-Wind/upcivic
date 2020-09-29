24a1270e931a90a489cdf6777d290801dbddbfc4
Greg Intermaggio <g.intermaggio@gmail.com>
2020-09-28 17:09:07 -0700

    Create / Edit / Archive tasks

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreTask;
+use App\Http\Requests\UpdateTask;
 use App\Organization;
 use App\Task;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
@@ -14,13 +18,7 @@ class TaskController extends Controller
      */
     public function index()
     {
-        $tasks = [
-            'Assigned staff must provide negative TB test result',
-            'Sign a contract',
-            'Provide a local business license',
-            'Provide liability insurance policy',
-            'Provide an affidavit'
-        ];
+        $tasks = Task::all();
         $organizations = Organization::all();
         return view('tenant.admin.tasks.index', compact('tasks', 'organizations'));
     }
@@ -30,10 +28,17 @@ class TaskController extends Controller
         return view('tenant.admin.tasks.create');
     }
 
-    public function store()
+    public function store(StoreTask $request)
     {
-        dd('heyo');
-        return;
+        $validated = $request->validated();
+        $task = Task::make([
+            'name' => $validated['name'],
+            'description' => $validated['description'],
+            ]);
+        $task->organization_id = tenant()->organization->id;
+        $task->assign_to_entity = $validated['assignToEntity'];
+        $task->save();
+        return redirect()->route('tenant:admin.tasks.index', [tenant()->slug])->withSuccess('Task created successfully.');
     }
 
     public function edit(Task $task)
@@ -41,6 +46,29 @@ class TaskController extends Controller
         //
 
         return view('tenant.admin.tasks.edit', compact('task'));
+    }
+
+
+    public function update(Task $task, UpdateTask $request)
+    {
+        //
+        $validated = $request->validated();
+        $task->update([
+            'name' => $validated['name'],
+            'description' => $validated['description'],
+            ]);
+
+        return redirect()->route('tenant:admin.tasks.index', tenant()['slug'])->withSuccess('Task has been updated.');
+    }
+
+    public function archive(Task $task)
+    {
+        //
+
+        $task->archived_at = Carbon::now();
+        $task->save();
+
+        return redirect()->route('tenant:admin.tasks.index', tenant()['slug'])->withSuccess('Task has been archived.');
     }
 
     public function destroy(Task $task)

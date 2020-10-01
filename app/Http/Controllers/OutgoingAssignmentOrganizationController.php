c8de64b443c5b176886747b86fb83565ef13c390
Greg Intermaggio <g.intermaggio@gmail.com>
2020-10-01 07:44:05 -0700

    Fix scoping issues-- only show instructors of the relevant organization instead of showing all, and list all partnered organizations, even those with no assignments.

--- app/Http/Controllers/OutgoingAssignmentOrganizationController.php
+++ app/Http/Controllers/OutgoingAssignmentOrganizationController.php
@@ -11,7 +11,7 @@ class OutgoingAssignmentOrganizationController extends Controller
     public function index(Organization $organization)
     {
         $isOutgoingFromTenant = true;
-        $instructors = Instructor::assignedToOrganization(tenant()->organization_id)->get();
+        $instructors = $organization->instructors()->assignedToOrganization(tenant()->organization_id)->get();
         return view('tenant.admin.assignments.organizations.index', compact('organization', 'isOutgoingFromTenant', 'instructors'));
     }
 

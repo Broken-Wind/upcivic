1a36e00fbcb8b204f54c4f654db6706002a1fb70
calinf <calin.furau@gmail.com>
2020-10-05 20:34:21 -0400

    Add reusable file_list component

--- resources/views/tenant/admin/assignments/organizations/components/assignment_list.blade.php
+++ resources/views/tenant/admin/assignments/organizations/components/assignment_list.blade.php
@@ -1,14 +1,14 @@
 <container>
     @forelse($assignments as $assignment)
     <div class="row">
-        <div class="col-9">
+        <div class="col-10">
             <a href="{{ tenant()->route('tenant:admin.assignments.edit', ['assignment' => $assignment->id]) }}">
                 {{ $assignment->name }} 
             </a>
             <i class="fas fa-info-circle" title="{{ $assignment->description }}"></i>
         </div>
 
-        <div class="col-3">
+        <div class="col-2">
             <div class="{{ $assignment->class_string }} p-1 font-weight-bold text-center mr-2 px-3">
                 {{ $assignment->status_string }}
             </div>

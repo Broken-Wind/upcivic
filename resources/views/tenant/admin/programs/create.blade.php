6ef5743fb87b3922c58deb6a54fd006321630c31
calinf <calin.furau@gmail.com>
2020-10-10 12:14:24 -0400

    Re-add [VIRTUAL] site to the sites list when proposing a program.

--- resources/views/tenant/admin/programs/create.blade.php
+++ resources/views/tenant/admin/programs/create.blade.php
@@ -42,12 +42,10 @@
                         <select class="form-control" name="site_id" id="">
                             <option value="">Unknown - To be determined</option>
                             @foreach ($sites as $site)
-                                @if ($site['name'] != '[VIRTUAL]')
-                                    <option value="{{ $site['id'] }}">{{ $site['name'] }} - {{ $site['address'] }}</option>
-                                @endif
+                                <option value="{{ $site['id'] }}">{{ $site['name'] }} - {{ $site['address'] }}</option>
                             @endforeach
                         </select>
-                        <small id="add-site" class="text-muted">Offsite and Virtual sites are added automatically. Can't find the site you'd like? <a href="" data-toggle="modal" data-target="#add-site-modal">Add a site</a></small>
+                        <small id="add-site" class="text-muted">For virtual programs, select [VIRTUAL]. Can't find the site you'd like? <a href="" data-toggle="modal" data-target="#add-site-modal">Add a site</a></small>
                     </div>
                 </div>
                 <div class="form-group">

ecbd82859240b313f1ac36a39280c07e32c4925c
Greg Intermaggio <g.intermaggio@gmail.com>
2020-12-28 18:29:55 -0800

    X for removing organizations from bulk assignment, and hide it when only one org is receiving an assignment.

--- resources/views/tenant/admin/assignments/components/organization_assignment_preview.blade.php
+++ resources/views/tenant/admin/assignments/components/organization_assignment_preview.blade.php
@@ -6,11 +6,13 @@
             <h3>{{ $organization->name }}</h3>
             <input type="hidden" name="organization_ids[]" value="{{ $organization->id }}">
         </div>
-        <div class="col-auto">
-            <a href="{{ route('tenant:admin.assignments.review', ['tenant' => tenant()->slug, 'task_id' => $task->id, 'organization_ids' => $organizations->except($organization->id)->pluck('id')->toArray()]) }}" class="btn btn-danger" onClick="return confirm('Are you sure you don\'t want to assign the task to this organization?');">
-                Cancel Assignment
-            </a>
-        </div>
+        @if($organizations->count() > 1)
+            <div class="col-auto">
+                <a href="{{ route('tenant:admin.assignments.review', ['tenant' => tenant()->slug, 'task_id' => $task->id, 'organization_ids' => $organizations->except($organization->id)->pluck('id')->toArray()]) }}" class="btn btn-danger" onClick="return confirm('Are you sure you don\'t want to assign the task to this organization?');">
+                    <i class="fas fa-fw fa-times"></i>
+                </a>
+            </div>
+        @endif
     </div>
     @if($task->isSignableDocument())
         <table class="table table-striped table-sm mb-4">

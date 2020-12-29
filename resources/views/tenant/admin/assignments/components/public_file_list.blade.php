e7909b465072669e5521c7618ccca52143f89cce
calinf <calin.furau@gmail.com>
2020-12-28 23:16:42 -0500

    Sign public download URL.

--- resources/views/tenant/admin/assignments/components/public_file_list.blade.php
+++ resources/views/tenant/admin/assignments/components/public_file_list.blade.php
@@ -2,7 +2,7 @@
     <div class="row my-1">
         <div class="mr-1">From {{ $organizationName }}: </div>
         @forelse($files as $file)
-            <a href="{{ tenant()->route('tenant:assignments.public.download', [$file]) }}" class="ml-1">
+            <a href="{{ URL::signedRoute('tenant:assignments.public.download', [tenant()->slug, $file]) }}" class="ml-1">
                 {{ $file->filename }} <i class="fas fa-download mr-1"></i>
             </a>
             @if(!$loop->last) | @endif

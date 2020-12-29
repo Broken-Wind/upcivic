a3943c9bf911468a7a6eca02dfe9e88aaaf1f3f5
Greg Intermaggio <g.intermaggio@gmail.com>
2020-12-29 08:43:36 -0800

    Text changes on document_signed email

--- resources/views/emails/document_signed.blade.php
+++ resources/views/emails/document_signed.blade.php
@@ -2,12 +2,12 @@
 {{ $organization->name }} has signed the document listed below via {{ config('app.name') }}.
 
 @component('mail::panel')
-## {{ $organization->name }} signed {{ $assignment['name'] }}
+## {{ $assignment['name'] }}
 {{ $assignment['description'] }}
 @endcomponent
 
 @component('mail::button', ['url' => $signedUrl])
-View Details on Upcivic
+View Details on {{ config('app.name') }}
 @endcomponent
 
 @endcomponent

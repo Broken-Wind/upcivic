5d3beb59c594f0ba0840f64167374f9778edd0bb
calinf <calin.furau@gmail.com>
2021-01-13 15:52:12 -0500

    Include the list of contributors in program summary.

--- resources/views/tenant/admin/programs/components/summary.blade.php
+++ resources/views/tenant/admin/programs/components/summary.blade.php
@@ -1,9 +1,16 @@
 <div class="row">
-    <div class="col-6">
+    <div class="col-12">
         <p class="lead">
             #{{ $program['id'] }} - {{ $program['name'] }} at {{ $program['site']['name'] }}<br/>
-            {{ $program['description_of_meetings'] }}<br/>
-            {{ $program['start_time'] }}-{{ $program['end_time'] }}
+            {{ $program['description_of_meetings'] }}
+            {{ $program['start_time'] }}-{{ $program['end_time'] }}<br/>
+            @foreach ($program['contributors'] as $contributor)
+                @if(!$loop->last)
+                    {{ $contributor->name }},
+                @else
+                    {{ $contributor->name }}
+                @endif
+            @endforeach
         </p>
     </div>
 </div>

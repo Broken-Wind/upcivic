2c2bdc1de23e7b7adaf51dc1765748ac437a1526
Greg Intermaggio <g.intermaggio@gmail.com>
2021-01-18 17:42:20 -0800

    Styling improvements for subscription checkout

--- resources/views/tenant/admin/payments.blade.php
+++ resources/views/tenant/admin/payments.blade.php
@@ -2,26 +2,46 @@
 @section('title', 'Subscription')
 @section('content')
 
+<div class="container">
+    @include('shared.form_errors')
+    <div class="card mb-4">
+        <div class="card-header">Upgrade to Pro</div>
+        <div class="card-body">
+            <!-- Stripe Elements Placeholder -->
+            <div id="card-element"></div>
 
-<label for="card-element">
-    Credit or Debit Card
-</label>
+            <div class="input-group mt-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text">Number of Seats</span>
+                </div>
+                <input class="form-control" type="number" name="noOfSeats" id="noOfSeats" aria-describedby="helpId" value="3" min="1" max="100" aria-label="The maximum number of users from your organization on Upcivic.">
+                <div class="input-group-append">
+                    <span class="input-group-text">x $49/month</span>
+                </div>
+            </div>
+            <small id="helpId" class="form-text text-muted">The maximum number of users from your organization on Upcivic.</small>
 
-<!-- Stripe Elements Placeholder -->
-<div id="card-element"></div>
+            <div class="form-group form-inline mt-4">
+                <label><strong>Total Monthly Cost: $<span id="total-cost">147</span>.00</strong></label>
+                <button id="card-button" class="btn btn-primary ml-3" data-secret="{{ $intent->client_secret }}">
+                    Subscribe
+                </button>
+            </div>
+        </div>
+    </div>
 
-Number of Seats:
-<input type="text" id="noOfSeats" name="noOfSeats" value="3" min="1" max="100">
-
-
-<button id="card-button" data-secret="{{ $intent->client_secret }}">
-    Subscribe
-</button>
+</div>
 
 <script type="application/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script type="application/javascript" src="https://js.stripe.com/v3/"></script>
 
 <script type="application/javascript">
+    const seatsElem = document.querySelector('#noOfSeats');
+    seatsElem.addEventListener('input', function (e) {
+        if (Number.isInteger(parseInt(e.target.value))) {
+            document.querySelector('#total-cost').innerHTML = parseInt(e.target.value) * 49;
+        }
+    });
     const stripe = Stripe('pk_test_51I9XCwGuEpAR4AJ4vR7GrbA4AXqHKjEpQPPMNxYbBbJzjwa9pDkXe0HsqB57CT5JUlran00D4gN5tAosPmO2GWKQ00shLzd316');
 
     const elements = stripe.elements();

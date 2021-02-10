7d6ac4fbe95b87800d7fbb49818a7a203a45ea2b
Greg Intermaggio <g.intermaggio@gmail.com>
2021-02-10 10:22:55 -0800

    Fix OrganizationTenantTest

--- tests/Feature/OrganizationTenantTest.php
+++ tests/Feature/OrganizationTenantTest.php
@@ -1,54 +1,39 @@
 <?php
-
 namespace Tests\Feature;
-
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Organization;
 use App\User;
-
 class OrganizationTenantTest extends TestCase
 {
     use RefreshDatabase;
-
     /** @test */
     public function organization_tenant_create_view_content_test()
     {
         $user = factory(User::class)->create();
-
         $organization = factory(Organization::class)->create();
 
         $response = $this->actingAs($user)->followingRedirects()->get("/organizations/{$organization->id}/tenant");
 
         $response->assertStatus(200);
-
         $this->assertEquals(url()->current(), route('organizations.tenant.create', $organization));
-
         $response->assertSeeText("Since {$organization->name} is unclaimed, you may select its vanity URL below.");
-
         $response->assertSeeText('Not your organization? Go home.');
-
         $response->assertSeeText('Confirm your Organization');
-
         $response->assertSee(route('organizations.tenant.store', $organization));
     }
-
     /** @test */
     public function can_store_organization_tenant()
     {
         $user = factory(User::class)->create();
-
         $organization = factory(Organization::class)->create(['name' => 'Dat Org']);
 
         $response = $this->actingAs($user)->followingRedirects()->post("/organizations/{$organization->id}/tenant", ['slug' => 'yaas']);
 
         $response->assertStatus(200);
-
-        $this->assertEquals(url()->current(), route('tenant:admin.home', $organization->tenant->slug));
-
+        $this->assertEquals(url()->current(), route('tenant:admin.programs.index', $organization->tenant->slug));
         $response->assertSeeText('Dat Org');
-
-        $response->assertSeeText('Welcome to '.config('app.name'));
+        $response->assertSeeText('Add program');
     }
 }

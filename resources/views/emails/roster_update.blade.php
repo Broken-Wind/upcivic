f8952cb41ff0fb7f2c2b49862175068aca74ecf1
Greg Intermaggio <g.intermaggio@gmail.com>
2021-03-03 13:56:06 -0800

    Remove button from roster_update mailable and update language to match

--- resources/views/emails/roster_update.blade.php
+++ resources/views/emails/roster_update.blade.php
@@ -1,15 +1,11 @@
 @component('mail::message')
-There is an updated roster for this program.
+The roster, sign-in sheet, and daily attendance sheets for this program have been updated due to a change in enrollment. Please find the updated documents attached.
 
 @component('mail::panel')
 ## {{ $program['name'] . " at " . $program->site['name'] }}
 * {{ $program['description_of_meetings'] }}<br />
 * {{ $program['start_time'] }}-{{ $program['end_time'] }}
 * {{ $program['description_of_age_range'] }}
-@endcomponent
-
-@component('mail::button', ['url' => route('root')])
-View Roster on {{ config('app.name') }}
 @endcomponent
 
 @endcomponent
